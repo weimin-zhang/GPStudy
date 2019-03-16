55c796e8df698b16d2c8f6c22b0b01e3f0c7760a
weimin_zhang <290294107@qq.com>
2019-03-16 13:31:24 +0800

    ①提交抽象工厂测试类

---
+++ src/main/java/com/weimin/desiginpattern/factory/abstractfactory/AbstractFactoryTest.java
@@ -0,0 +1,20 @@
+package com.weimin.desiginpattern.factory.abstractfactory;
+
+/**
+ * AbstractFactoryTest class
+ *
+ * @author
+ * @date
+ */
+public class AbstractFactoryTest {
+    public static void main(String[] args) {
+        LuxuryCarFactory luxuryCarFactory = new LuxuryCarFactory();
+        Engine engine = luxuryCarFactory.createEngine();
+        Seat seat = luxuryCarFactory.createSeat();
+        Tyre tyre = luxuryCarFactory.createTyre();
+        engine.run();
+        engine.start();
+        seat.massage();
+        tyre.revolve();
+    }
+}
